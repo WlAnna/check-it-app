02602b291c63629126bfcafa6178ca440a1168cb
Anna W <ania_4508@tlen.pl>
2020-10-16 16:43:44 +0100

    add time and city name

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,30 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 const radioUnit = document.querySelector('#r')
 const weatherIcon = document.querySelector('#weather-icon')
+const time = document.querySelector('#time')
+const cityName = document.querySelector('#location')
 
 
+fetch("http://ip-api.com/json/?fields=status,city").then((response) => {
+    response.json().then((data) => {
+        if(data.status  === "success") {
+            cityName.textContent = data.city
+        } 
+    })
+})
+
+
+const date = new Date(1602871200*1000)
+console.log(date)
+time.textContent = date.toLocaleString()
+
+function changeButton() {
+    if(document.getElementById('f').checked === true) {
+        return unit = document.getElementById('f').value
+    } else {
+        return unit = document.getElementById('m').value
+    }
+}
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -17,7 +39,8 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent =''
 
-    let unit = 'm'
+
+    let unit = changeButton()
 
 
     fetch('/weather?address=' + location + '&unit=' + unit).then((response) => {
